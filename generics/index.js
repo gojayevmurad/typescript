6f583e6f02ca94833cf57d02206ffee725f291cc
Murad Gojayev <muradqocayevi20@gmail.com>
2023-08-28 01:05:48 +0400

    completed

---
+++ generics/index.js
@@ -0,0 +1,24 @@
+/*
+    function getValue(x: number): number {
+    return x;
+    }
+
+    function getValue2(x: string): string {
+    return x;
+    }
+
+    let value = getValue(10);
+
+    console.log(value);
+
+    let city = getValue2("Baku");
+
+    console.log(city);
+*/
+function getValue(x) {
+    return x;
+}
+var num = getValue(2);
+console.log(num);
+var city3 = getValue("s");
+console.log(city3);
--- generics/index.ts
+++ generics/index.ts
@@ -1,4 +1,38 @@
-function getValue(x: number): number {
+//#region variant 1
+/*
+    function getValue(x: number): number {
+    return x;
+    }
+
+    function getValue2(x: string): string {
+    return x;
+    }
+
+    let value = getValue(10);
+
+    console.log(value);
+
+    let city = getValue2("Baku");
+
+    console.log(city);
+*/
+//#endregion
+
+function getValue<T>(x: T): T {
   return x;
 }
 
+let num = getValue<number>(2);
+console.log(num);
+
+let city3 = getValue<string>("s");
+console.log(city3);
+
+class GenericClass<T> {
+  data: T;
+  getData(param: T): T {
+    return param;
+  }
+}
+
+let newClass = new GenericClass<number>();
